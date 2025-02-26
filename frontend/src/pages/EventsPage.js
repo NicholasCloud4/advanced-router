d1b5f291ce2b7843a056b180b25220da1b12ecdb
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-02-26 14:09:53 -0500

    Data Fetching with a loader()

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,30 +1,30 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from 'react';
 
-const DUMMY_EVENTS = [
-    {
-        id: 'e1',
-        title: 'Programming for Everyone',
-    },
-    {
-        id: 'e2',
-        title: 'Networking for Coders',
-    }
+import EventsList from '../components/EventsList';
 
-]
+function EventsPage() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedEvents, setFetchedEvents] = useState();
+    const [error, setError] = useState();
 
-export default function EventsPage() {
+    useEffect(() => {
+        async function fetchEvents() {
+            setIsLoading(true);
+
+            setIsLoading(false);
+        }
+
+        fetchEvents();
+    }, []);
     return (
         <>
-            <h1>EventsPage</h1>
-            <ul>
-                {DUMMY_EVENTS.map((event) => {
-                    return (
-                        <li key={event.id}>
-                            <Link to={event.id}>{event.title}</Link>
-                        </li>
-                    )
-                })}
-            </ul>
+            <div style={{ textAlign: 'center' }}>
+                {isLoading && <p>Loading...</p>}
+                {error && <p>{error}</p>}
+            </div>
+            {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
         </>
-    )
+    );
 }
+
+export default EventsPage;
