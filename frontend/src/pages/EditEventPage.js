45ec65a31582ff5e16f51d9861777d55a8f4ffdf
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-02-26 19:19:04 -0500

    AccessignData from other routes

--- frontend/src/pages/EditEventPage.js
+++ frontend/src/pages/EditEventPage.js
@@ -1,6 +1,9 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
 
 export default function EditEventPage() {
+    const data = useRouteLoaderData("event-detail");
     return (
-        <h1>EditEventPage</h1>
+        <EventForm event={data.event} />
     )
 }
